d98f2db9f213f00d8a262c40a1aa6a893149af47
Sidharth Shanker <sshanker220@gmail.com>
2012-07-15 15:21:04 -0700

    Added mongo to project

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject mass-todo "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [congomongo "0.1.5-SNAPSHOT"]]
             :main mass-todo.server)
 
