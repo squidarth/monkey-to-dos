d133ce60b09d59850884dc862c12b5bb9a3d5018
Sidharth Shanker <sshanker220@gmail.com>
2012-07-15 22:32:42 -0700

    Changed font

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
-                           [congomongo "0.1.5-SNAPSHOT"]]
+                           [congomongo "0.1.9"]]
             :main mass-todo.server)
 
